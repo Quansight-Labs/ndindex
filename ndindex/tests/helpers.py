e685c4f668b0a1f9373558ae8eb080f39885189c
Aaron Meurer <asmeurer@gmail.com>
2020-03-11 16:14:42 -0600

    Allow disabling the same exception test in check_same

--- ndindex/tests/helpers.py
+++ ndindex/tests/helpers.py
@@ -2,7 +2,7 @@ from numpy.testing import assert_equal
 
 from pytest import fail
 
-def check_same(a, raw_type, raw_args, idx_type, idx_args, raises=False):
+def check_same(a, raw_type, raw_args, idx_type, idx_args, same_exception=True):
     exception = None
     try:
         raw = raw_type(*raw_args)
@@ -15,12 +15,13 @@ def check_same(a, raw_type, raw_args, idx_type, idx_args, raises=False):
         a_idx = a[idx.raw]
     except Exception as e:
         if not exception:
-            fail(f"Raw form does not raise but ndindex form does ({raw_type}(*{raw_args}))")
-        assert type(e) == type(exception)
-        assert e.args == exception.args, (e.args, exception.args)
+            fail(f"Raw form does not raise but ndindex form does ({e!r}): {raw_type.__name__}(*{raw_args})")
+        if same_exception:
+            assert type(e) == type(exception), (e, exception)
+            assert e.args == exception.args, (e.args, exception.args)
     else:
         if exception:
-            fail(f"ndindex form did not raise but raw form does ({idx_type}(*{idx_args}))")
+            fail(f"ndindex form did not raise but raw form does ({exception!r}): {idx_type.__name__}(*{idx_args})")
 
     if not exception:
         assert_equal(a_raw, a_idx)
