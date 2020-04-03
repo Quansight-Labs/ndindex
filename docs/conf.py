8b7c076237c80ee22c1752493bdf0412b9b72e9d
Aaron Meurer <asmeurer@gmail.com>
2020-04-03 17:34:13 -0600

    Enable nitpicky = True in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,12 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+# Make warnings errors
+
+# Make sphinx give errors for bad cross-references
+nitpicky = True
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
