3d74f0d3234bef8e9c1fc3ca03c7eac456c8709e
Aaron Meurer <asmeurer@gmail.com>
2020-05-29 22:21:40 -0600

    Enable equation numbering in MathJax formulas in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -106,6 +106,13 @@ html_sidebars = {
     '**': ['globaltocindex.html', 'searchbox.html'],
 }
 
+mathjax_config = {
+  'TeX': {
+      'equationNumbers': {
+          'autoNumber': "AMS",
+      },
+  },
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
