f798f61c6e8a135302d5e76b75bfc71a7dc29716
Aaron Meurer <asmeurer@gmail.com>
2020-06-17 16:02:12 -0600

    Add a command line flag to pytest to set the hypothesis max examples

--- conftest.py
+++ conftest.py
@@ -3,3 +3,28 @@ import sys
 sys.path.insert(0, '.')
 
 collect_ignore = ["setup.py", "docs/conf.py"]
+
+# Add a --hypothesis-max-examples flag to pytest. See
+# https://github.com/HypothesisWorks/hypothesis/issues/2434#issuecomment-630309150
+
+def pytest_addoption(parser):
+    # Add an option to change the Hypothesis max_examples setting.
+    parser.addoption(
+        "--hypothesis-max-examples",
+        action="store",
+        default=None,
+        help="set the Hypothesis max_examples setting",
+    )
+
+
+def pytest_configure(config):
+    # Set Hypothesis max_examples.
+    hypothesis_max_examples = config.getoption("--hypothesis-max-examples")
+    if hypothesis_max_examples is not None:
+        import hypothesis
+
+        hypothesis.settings.register_profile(
+            "hypothesis-overridden", max_examples=int(hypothesis_max_examples)
+        )
+
+        hypothesis.settings.load_profile("hypothesis-overridden")
