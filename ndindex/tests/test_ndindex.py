401354441b836b89d63a43823f4c73c86ea58569
Aaron Meurer <asmeurer@gmail.com>
2020-08-05 16:43:44 -0600

    Revert "Add a test that will fail coverage"
    This reverts commit f2147204e029fe9f6d0e2cb6bc2a9b42bb814e0a.

--- ndindex/tests/test_ndindex.py
+++ ndindex/tests/test_ndindex.py
@@ -109,6 +109,3 @@ def test_asshape():
     raises(TypeError, lambda: asshape(...))
     raises(TypeError, lambda: asshape(Integer(1)))
     raises(TypeError, lambda: asshape(Tuple(1, 2)))
-
-def no_coverage():
-    pass
