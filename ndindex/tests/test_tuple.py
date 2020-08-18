d3c7d9343e4d573d1da33261ff11d947dc13123d
Aaron Meurer <asmeurer@gmail.com>
2020-08-18 17:32:18 -0600

    Remove the duplicate test_tuple_expand_hypothesis() test
    The test_ndindex_expand_hypothesis() already tests tuples because the
    ndindices strategy generates tuples, and it already tests all the same things
    that the test_tuple_expand_hypothesis test tests.

--- ndindex/tests/test_tuple.py
+++ ndindex/tests/test_tuple.py
@@ -153,33 +153,6 @@ def test_tuple_reduce_explicit():
 @example((..., [0, 1], 0), (2, 2))
 @example((..., None, 0), 1)
 @example((0, 1, ..., 2, 3), (2, 3, 4, 5, 6, 7))
-@given(Tuples, one_of(shapes, integers(0, 10)))
-def test_tuple_expand_hypothesis(t, shape):
-    if isinstance(shape, int):
-        a = arange(shape)
-    else:
-        a = arange(prod(shape)).reshape(shape)
-
-    index = Tuple(*t)
-
-    try:
-        expanded = index.expand(shape)
-    except (IndexError, NotImplementedError):
-        pass
-    else:
-        assert isinstance(expanded, Tuple)
-        assert ... not in expanded.args
-        n_newaxis = index.args.count(None)
-        if isinstance(shape, int):
-            assert len(expanded.args) == 1 + n_newaxis
-        else:
-            assert len(expanded.args) == len(shape) + n_newaxis
-
-    check_same(a, index.raw, ndindex_func=lambda a, x: a[x.expand(shape).raw],
-               same_exception=False)
-
-# This is here because expand() always returns a Tuple, so it is very similar
-# to the test_tuple_expand_hypothesis test.
 @example(None, 2)
 @given(ndindices, one_of(shapes, integers(0, 10)))
 def test_ndindex_expand_hypothesis(idx, shape):
