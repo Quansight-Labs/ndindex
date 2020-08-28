6c1e88f98f33bcca2690eb340ab91359517004e1
Aaron Meurer <asmeurer@gmail.com>
2020-08-28 11:15:19 -0600

    Check if NumPy is new enough when running the tests

--- conftest.py
+++ conftest.py
@@ -1,8 +1,14 @@
 # Make sure the doctests in the docs can import the module
 import sys
 sys.path.insert(0, '.')
+from distutils.version import LooseVersion
 
 from hypothesis import settings
+
+# Make sure a new enough version of NumPy is installed for the tests
+import numpy
+if LooseVersion(numpy.__version__) < '1.20':
+    raise RuntimeError("NumPy 1.20 (development version) or greater is required to run the ndindex tests")
 
 # Add a --hypothesis-max-examples flag to pytest. See
 # https://github.com/HypothesisWorks/hypothesis/issues/2434#issuecomment-630309150
