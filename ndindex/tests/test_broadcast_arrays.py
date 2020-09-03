afe435816320adb0b8757644076c3f0f47ddab51
Aaron Meurer <asmeurer@gmail.com>
2020-09-03 15:15:59 -0600

    Add an @example for coverage

--- ndindex/tests/test_broadcast_arrays.py
+++ ndindex/tests/test_broadcast_arrays.py
@@ -11,6 +11,7 @@ from ..integer import Integer
 from ..tuple import Tuple
 from .helpers import ndindices, check_same, short_shapes
 
+@example((..., False, False), 1)
 @example((True, False), 1)
 @example((True, True), 1)
 @example(array([False]), 0)
