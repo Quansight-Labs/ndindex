8389bdb19ffb71a0ec0a49aabbd27a0611d375e8
Aaron Meurer <asmeurer@gmail.com>
2020-12-18 17:27:15 -0700

    Fix MathJax in the latest version of MyST

--- docs/conf.py
+++ docs/conf.py
@@ -118,6 +118,8 @@ mathjax_config = {
   },
 }
 
+myst_update_mathjax=False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
