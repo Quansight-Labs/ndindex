aede2b468501897693042ef16b85d23f66aa5d72
Aaron Meurer <asmeurer@gmail.com>
2021-04-05 16:07:42 -0600

    Make the update_benchmarks.sh script push gh-pages when it is done

--- update_benchmarks.sh
+++ update_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Run and update the benchmarks in the gh-pages branch. This commits to that
-# branch but does not push. The resulting benchmarks are served at
+# Run and update the benchmarks in the gh-pages branch. The resulting
+# benchmarks are served at
 # https://quansight-labs.github.io/ndindex/benchmarks/index.html
 set -e
 set -x
@@ -14,3 +14,4 @@ cp -R .asv/html benchmarks
 git add benchmarks
 git commit -m "Update benchmarks"
 git checkout -
+git push gh-pages
