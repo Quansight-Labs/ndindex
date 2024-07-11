890dfe30a0f946328b0f07ef6d3b4e5f24cde0c1
Aaron Meurer <asmeurer@gmail.com>
2024-07-11 16:10:13 -0600

    Make pybind11 implementation faster

--- simple_slice_pybind11.cpp
+++ simple_slice_pybind11.cpp
@@ -1,7 +1,5 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
-#include <tuple>
-#include <stdexcept>
 #include <limits>
 
 namespace py = pybind11;
@@ -11,17 +9,19 @@ private:
     py::ssize_t _start;
     py::ssize_t _stop;
     py::ssize_t _step;
-    bool _has_start;
-    bool _has_stop;
-    bool _has_step;
+    uint8_t _flags; // Use bit flags instead of separate booleans
 
-    static py::ssize_t py_index(const py::handle& obj) {
-        if (py::isinstance<py::bool_>(obj)) {
-            throw py::type_error("'bool' object cannot be interpreted as an integer");
+    static constexpr uint8_t HAS_START = 1;
+    static constexpr uint8_t HAS_STOP = 2;
+    static constexpr uint8_t HAS_STEP = 4;
+
+    static inline py::ssize_t py_index(const py::handle& obj) {
+        if (PyLong_Check(obj.ptr())) {
+            return PyLong_AsSsize_t(obj.ptr());
         }
-        if (py::hasattr(obj, "__class__") && 
-            py::str(obj.attr("__class__").attr("__name__")).cast<std::string>() == "bool_") {
-            throw py::type_error("'numpy.bool_' object cannot be interpreted as an integer");
+        if (PyBool_Check(obj.ptr()) || (PyObject_HasAttrString(obj.ptr(), "__class__") &&
+            strcmp(PyUnicode_AsUTF8(PyObject_GetAttrString(PyObject_GetAttrString(obj.ptr(), "__class__"), "__name__")), "bool_") == 0)) {
+            throw py::type_error("boolean value cannot be interpreted as an integer");
         }
         PyObject* index = PyNumber_Index(obj.ptr());
         if (!index) {
@@ -36,21 +36,23 @@ private:
     }
 
 public:
-    SimpleSlicePybind11(py::object start, py::object stop = py::none(), py::object step = py::none()) {
+    SimpleSlicePybind11(py::handle start, py::handle stop = py::none(), py::handle step = py::none()) : _flags(0) {
         if (py::isinstance<SimpleSlicePybind11>(start)) {
             auto& other = start.cast<SimpleSlicePybind11&>();
             _start = other._start;
             _stop = other._stop;
             _step = other._step;
-            _has_start = other._has_start;
-            _has_stop = other._has_stop;
-            _has_step = other._has_step;
+            _flags = other._flags;
             return;
         }
 
-        if (py::isinstance<py::slice>(start)) {
-            py::slice s = start.cast<py::slice>();
-            *this = SimpleSlicePybind11(s.attr("start"), s.attr("stop"), s.attr("step"));
+        if (PySlice_Check(start.ptr())) {
+            Py_ssize_t slice_start, slice_stop, slice_step;
+            PySlice_Unpack(start.ptr(), &slice_start, &slice_stop, &slice_step);
+            _start = slice_start;
+            _stop = slice_stop;
+            _step = slice_step;
+            _flags = HAS_START | HAS_STOP | HAS_STEP;
             return;
         }
 
@@ -58,27 +60,35 @@ public:
             std::swap(start, stop);
         }
 
-        _has_start = !start.is_none();
-        _has_stop = !stop.is_none();
-        _has_step = !step.is_none();
+        if (!start.is_none()) {
+            _start = py_index(start);
+            _flags |= HAS_START;
+        }
 
-        _start = _has_start ? py_index(start) : 0;
-        _stop = _has_stop ? py_index(stop) : 0;
-        _step = _has_step ? py_index(step) : 1;
+        if (!stop.is_none()) {
+            _stop = py_index(stop);
+            _flags |= HAS_STOP;
+        }
 
-        if (_has_step && _step == 0) {
-            throw py::value_error("slice step cannot be zero");
+        if (!step.is_none()) {
+            _step = py_index(step);
+            if (_step == 0) {
+                throw py::value_error("slice step cannot be zero");
+            }
+            _flags |= HAS_STEP;
+        } else {
+            _step = 1;
         }
     }
 
     py::object get_start() const { 
-        return _has_start ? py::cast(_start) : py::none();
+        return _flags & HAS_START ? py::cast(_start) : py::none();
     }
     py::object get_stop() const { 
-        return _has_stop ? py::cast(_stop) : py::none();
+        return _flags & HAS_STOP ? py::cast(_stop) : py::none();
     }
     py::object get_step() const { 
-        return _has_step ? py::cast(_step) : py::none();
+        return _flags & HAS_STEP ? py::cast(_step) : py::none();
     }
     py::tuple get_args() const { 
         return py::make_tuple(get_start(), get_stop(), get_step());
@@ -86,21 +96,20 @@ public:
 
     py::object raw() const {
         return py::reinterpret_steal<py::object>(PySlice_New(
-            get_start().ptr(),
-            get_stop().ptr(),
-            get_step().ptr()
+            (_flags & HAS_START) ? PyLong_FromSsize_t(_start) : Py_None,
+            (_flags & HAS_STOP) ? PyLong_FromSsize_t(_stop) : Py_None,
+            (_flags & HAS_STEP) ? PyLong_FromSsize_t(_step) : Py_None
         ));
     }
 
     bool operator==(const SimpleSlicePybind11& other) const {
-        return _start == other._start && _stop == other._stop && _step == other._step &&
-               _has_start == other._has_start && _has_stop == other._has_stop && _has_step == other._has_step;
+        return _start == other._start && _stop == other._stop && _step == other._step && _flags == other._flags;
     }
 };
 
 PYBIND11_MODULE(simple_slice_pybind11, m) {
     py::class_<SimpleSlicePybind11>(m, "SimpleSlicePybind11")
-        .def(py::init<py::object, py::object, py::object>(),
+        .def(py::init<py::handle, py::handle, py::handle>(),
              py::arg("start"), py::arg("stop") = py::none(), py::arg("step") = py::none())
         .def_property_readonly("start", &SimpleSlicePybind11::get_start)
         .def_property_readonly("stop", &SimpleSlicePybind11::get_stop)
