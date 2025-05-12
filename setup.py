8333e0316d8a5037ea18d105dbb6232db839ec36
Ralf Gommers <ralf.gommers@gmail.com>
2025-05-12 10:32:48 +0200

    Fix missing `build-backend` key in pyproject.toml and a deprecation warning
    The warning was:
    ```
    /Users/rgommers/mambaforge/envs/ndindex/lib/python3.13/site-packages/setuptools/dist.py:759: SetuptoolsDeprecationWarning: License classifiers are deprecated.
    !!
    
            ********************************************************************************
            Please consider removing the following classifiers in favor of a SPDX license expression:
    
            License :: OSI Approved :: MIT License
    
            See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
            ********************************************************************************
    
    !!
    ```

--- setup.py
+++ setup.py
@@ -75,7 +75,6 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.9',
